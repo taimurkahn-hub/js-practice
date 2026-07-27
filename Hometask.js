d1b8cb42d981194e0fc8bc1f11a3b37fa7a96ad6
Taimur Khan <taimurkhn0910@gmail.com>
2026-07-27 15:25:41 +0500

    Feat: Task 2 Done

--- Hometask.js
+++ Hometask.js
@@ -1,5 +1,14 @@
+//Home Task 01
 const convertTempreture = (temp) => {
     temp = (temp*9/5)+32;
     return temp;
 }
 console.log (`${convertTempreture(40)} fahrenheit`)
+
+// Task 02
+
+const applyDiscount = function (price , DiscountPercentage) {
+    const discountAmount = (DiscountPercentage * price) / 100;
+    return price-discountAmount;
+}
+console.log(applyDiscount(1000 , 10))
