b567509302f3663a56deaf0c59e94f346aca59b0
Taimur Khan <taimurkhn0910@gmail.com>
2026-07-27 14:05:08 +0500

    Feat: Task 02 Done

--- classtask.js
+++ classtask.js
@@ -1,3 +1,4 @@
+// Class Task 01 
 function multiply (a,b){
     return a*b;
 }
@@ -5,3 +6,15 @@ console.log(multiply(20,5));
 
 const multiplication = (a,b) => a*b ;
 console.log(multiplication(5,7))
+
+//Class Task 02
+
+const checkPassGrade = function (a) {
+    if(a >=40){
+        return true;
+    } else {
+        return false;
+    }
+
+}
+console.log(checkPassGrade(30))
