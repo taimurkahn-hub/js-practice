446cb5f7ae4719389e225d978c6bcdb304fb4e08
Taimur Khan <taimurkhn0910@gmail.com>
2026-07-27 15:44:25 +0500

    Feat: Task 5 Done

--- Hometask.js
+++ Hometask.js
@@ -29,3 +29,16 @@ console.log(getInterestCatagory(500))
 //Task 04
 const convertDistanceUnit = (distance) => distance = distance * 0.621 ;
 console.log(convertDistanceUnit(20))
+
+//task 05
+function applyDiscount(price, discountPercent) {
+  return price - (price * (discountPercent / 100));
+}
+
+const prices = [50, 100, 150, 200];
+const discount = 20;
+
+for (const p of prices) {
+  const finalPrice = applyDiscount(p, discount);
+  console.log(`Original: $${p} -> Discounted (${discount}%): $${finalPrice}`);
+}
