3866a13271f7ea25eba2bcf0aba17dcd629585d9
Taimur Khan <taimurkhn0910@gmail.com>
2026-07-28 14:31:56 +0500

    feat: ForEach function

---
+++ arraysfunctions.js
@@ -0,0 +1,40 @@
+const students = [
+    {
+        id : 1, 
+        Name : "Taimur Khan",
+        Marks : 40,
+        Course : "Mern",
+
+    },
+        {
+        id : 2, 
+        Name : "Ahmed Khan",
+        Marks : 67,
+        Course : "Mern",
+
+    },
+        {
+        id : 3, 
+        Name : "Waqar Khan",
+        Marks : 89,
+        Course : "Mern",
+
+    },
+    {
+        id : 4, 
+        Name : "Waqas Khan",
+        Marks : 25,
+        Course : "Mern",
+
+    },
+]
+const numbers = [1,2,3,4,5,6,7,8];
+numbers.push("ABC");
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
+let totalMarks = 0;
+students.forEach((value) => {
+    totalMarks = value.Marks +totalMarks;
+})
+console.log(totalMarks);
