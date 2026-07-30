ecd97b99f117fa6232f1a0fbfc1eb53366321441
Taimur Khan <taimurkhn0910@gmail.com>
2026-07-30 11:15:19 +0500

    Feat: Completed Task

--- Hometask.js
+++ Hometask.js
@@ -3,6 +3,7 @@ const convertTempreture = (temp) => {
     temp = (temp*9/5)+32;
     return temp;
 }
+
 console.log (`${convertTempreture(40)} fahrenheit`)
 
 // Task 02
@@ -15,7 +16,7 @@ console.log(applyDiscount(1000 , 10))
 
 // Task3 
 
-function getInterestCatagory (creditScore){
+function getInterestCatagory (creditScore) {
     if(creditScore>=750){
         return "Low Risk"
     } else if(creditScore>=600){
@@ -31,7 +32,7 @@ const convertDistanceUnit = (distance) => distance = distance * 0.621 ;
 console.log(convertDistanceUnit(20))
 
 //task 05
-function applyDiscount(price, discountPercent) {
+function applyDiscount2(price, discountPercent) {
   return price - (price * (discountPercent / 100));
 }
 
@@ -39,7 +40,7 @@ const prices = [50, 100, 150, 200];
 const discount = 20;
 
 for (const p of prices) {
-  const finalPrice = applyDiscount(p, discount);
+  const finalPrice = applyDiscount2(p, discount);
   console.log(`Original: $${p} -> Discounted (${discount}%): $${finalPrice}`);
-  
+
 }
